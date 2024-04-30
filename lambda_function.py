3da43ceca9fb34d69659acdc9b5d385242379ee3
samuel.infantas@zup.com.br <samuel347@gmail.com>
2024-04-30 10:22:44 -0300

    fix: lambda

--- lambda_function.py
+++ lambda_function.py
@@ -33,12 +33,12 @@ def lambda_handler(event, context):
             connect_timeout_paramenter=5,
         )
 
-        query = f"SELECT * FROM {db_name}.city LIMIT 10"
-        # query = """INSERT INTO  decolagem.country
-        # (country_id, country_code, country_name, active_status, update_user_id, update_date)
-        # VALUES
-        # (246, 'NKA', 'Namekusei', False,'f6033e1d-86cf-4120-8c37-934d3c9797bf', CURRENT_TIMESTAMP())"""
+        query = f"SELECT * FROM {db_name}.table LIMIT 10"
         query_type = "read"
+
+        # query = f"""INSERT INTO  {db_name}.table
+        # (column1, column2, column3, column4, column5, column6, column7 )
+        # VALUES (val1, val2, val3, val4, val5, val6, val7)"""
         # query_type = "insert"
 
         df = query_execution_python(conn, query, query_type)
